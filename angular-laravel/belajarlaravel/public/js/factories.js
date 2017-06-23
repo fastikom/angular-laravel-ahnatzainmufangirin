43c4b36bb7627bf13007b9943115faa54f5bc030
ahnatzainmufangirin <ahmatzain13@gmail.com>
2017-06-23 13:11:06 +0700

    Delete factories.js

--- angular-laravel/belajarlaravel/public/js/factories.js
+++
@@ -1,9 +0,0 @@
-angular
-	.module('factories', [])
-	.factory('Mahasiswa', function($http){
-		return {
-			fetch: function(){
-				return $http.get(options.siteUrl + 'mahasiswa/fetch');
-			}
-		};
-	});
